ba88f6a9c7fb772aa108f24441314058935d8fa6
Coleman Cox <colezane82@gmail.com>
2015-08-11 22:43:12 -0500

    updating sum

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,18 +1,17 @@
-var Blog= Backbone.Model.extend({
+var Blog = Backbone.Model.extend({
   idAttribute: '_id',
   defaults: {
     title: '',
     body: '',
+    url: 'http://tiny-lr.herokuapp.com/collections/cc-blog',
     done: false
   }
-
 });
 
 var Bloggy = Backbone.Collection.extend({
   model: Blog,
-  url: 'http://tiny-lr.herokuapp.com/collections/cc-blog'
-   });
-    parse: function(data) {
-    return data.bloggy;
-  }
+  urlRoot: 'http://tiny-lr.herokuapp.com/collections/cc-blog'
 });
+
+return Bloggy;
+
