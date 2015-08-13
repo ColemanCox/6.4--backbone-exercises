6ada44644e33296396ebc926a19d91dbab901a99
Coleman Cox <colezane82@gmail.com>
2015-08-13 13:59:53 -0500

    sadasda

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,9 +1,10 @@
 var Blog = Backbone.Model.extend({
+  urlRoot: 'http://tiny-lr.herokuapp.com/collections/cc-blog',
   idAttribute: '_id',
   defaults: {
     title: '',
     body: '',
-    urlRoot: 'http://tiny-lr.herokuapp.com/collections/cc-blog',
+
     done: false
   }
 });
